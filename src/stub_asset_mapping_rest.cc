72dd985043f78dc587ef610c7c3abf20097acb1f
Jim Klimov <jim@jimklimov.com>
2020-04-23 10:48:54 +0200

    Bump copyrights to 2020

--- src/stub_asset_mapping_rest.cc
+++ src/stub_asset_mapping_rest.cc
@@ -1,7 +1,7 @@
 /*  =========================================================================
     stub_asset_mapping_rest - REST API for asset mapping
 
-    Copyright (C) 2018 - 2019 Eaton
+    Copyright (C) 2018 - 2020 Eaton
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
